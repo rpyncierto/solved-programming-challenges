10966ef49d54e4ad7ef8fb361179afd416e67e0d
Reymond Yncierto <ynciertorp@gmail.com>
2023-04-08 18:27:49 +0800

    added leetcode folder

--- leetcode/running-sum-of-1d-array.js
+++
@@ -1,8 +0,0 @@
-const runningSum = (nums) => {
-    let sum = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (i === 0) sum[i] = nums[i];
-        else sum[i] = sum[i-1] + nums[i];
-    }
-    return sum;
-};
