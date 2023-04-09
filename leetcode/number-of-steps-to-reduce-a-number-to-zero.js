97e170a3b635a717f7d65ed966d1a3333e88694d
Reymond Yncierto <ynciertorp@gmail.com>
2023-04-09 09:38:47 +0800

    solved number of steps to reduce a number to zero

---
+++ leetcode/number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,10 @@
+const numberOfSteps = num => {
+    let count = 0;
+    while (num != 0) {
+        if (num % 2 === 0) 
+            num /= 2;
+        else num -= 1;
+        count++;
+    }
+    return count;
+}
