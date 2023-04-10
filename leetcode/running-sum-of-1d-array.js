4b09fdaaf7783927801115392c77cc3257b1ac6f
Reymond Yncierto <ynciertorp@gmail.com>
2023-04-10 11:40:26 +0800

    refactor: optimized running sum of 1d array solution

--- leetcode/running-sum-of-1d-array.js
+++ leetcode/running-sum-of-1d-array.js
@@ -1,8 +1,8 @@
 const runningSum = (nums) => {
     let sum = [];
-    for (let i = 0; i < nums.length; i++) {
-        if (i === 0) sum[i] = nums[i];
-        else sum[i] = sum[i-1] + nums[i];
+    sum[0] = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        sum[i] = sum[i-1] + nums[i];
     }
     return sum;
 };
